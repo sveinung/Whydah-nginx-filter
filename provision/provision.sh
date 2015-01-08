890d6f08953c557534998fdd3fd062bbf157804d
Sveinung Dalatun <sdalatun@gmail.com>
2015-01-08 22:44:13 +0100

    Setup useradminservice

--- provision/provision.sh
+++ provision/provision.sh
@@ -3,5 +3,9 @@ set -e
 pushd /vagrant/provision
 
 sudo apt-get install make
+sudo apt-get install openjdk-7-jre-headless
+
+sudo cp start-user-admin-service.sh /home/vagrant
+sudo cp useradminservice.TEST.properties /home/vagrant
 
 popd
---
+++ provision/start-user-admin-service.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export IAM_MODE=TEST
+
+A=UserAdminService
+V=LATEST
+JARFILE=$A-$V.jar
+
+pkill -f $A
+
+wget --no-clobber --user=altran --password=l1nkSys -O $JARFILE "http://mvnrepo.cantara.no/service/local/artifact/maven/content?r=snapshots&g=net.whydah.identity&a=$A&v=$V&p=jar"
+nohup java -jar -DIAM_CONFIG=useradminservice.TEST.properties $JARFILE &
+
+tail -f nohup.out

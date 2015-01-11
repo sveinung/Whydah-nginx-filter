7698cca34bf58ffe81631e2a19561fd496785df1
Sveinung Dalatun <sdalatun@gmail.com>
2015-01-11 17:42:15 +0100

    Clean up provisioning

--- provision/provision.sh
+++ provision/provision.sh
@@ -9,13 +9,17 @@ sudo apt-get install openjdk-7-jre-headless
 
 mkdir -p $DIR/useradminservice
 chown vagrant:vagrant $DIR/useradminservice
+pushd useradminservice
 sudo cp start-user-admin-service.sh $DIR/useradminservice
 sudo cp useradminservice.TEST.properties $DIR/useradminservice
+popd
 
 mkdir -p $DIR/securitytokenservice
 chown vagrant:vagrant $DIR/securitytokenservice
+pushd securitytokenservice
 sudo cp update-security-token-service.sh $DIR/securitytokenservice
 sudo cp start-security-token-service.sh $DIR/securitytokenservice
 sudo cp securitytokenservice.TEST.properties $DIR/securitytokenservice
+popd
 
 popd
